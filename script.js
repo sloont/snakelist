e23c1b3c85f1179da5c287303ea996036ead5d95
Sloont <ColinAbel93@gmail.com>
2021-05-10 22:17:25 -0400

    set up the grid as a 40x40 and added the class pixel and a class for the x and y coordinate to each pixel div

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+let countX = 0;
+let countY = 0;
+
 for (let i = 0; i < (40*40); i++) {
     const game = document.getElementById("game");
     const pixel = document.createElement("div");
+
+    ++countX;
+    if (i !== 0 && i % 40 === 0) {
+        countY++;
+        countX = 0;
+    }
+    
     pixel.classList.add("pixel");
+    pixel.classList.add("x" + countX);
+    pixel.classList.add("y" + countY);
     game.appendChild(pixel);
+
+    
 }
